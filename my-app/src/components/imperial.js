d4c022708432a010a499f770dc431e4da6f0c1ce
onuri5 <jomibaofficial@gmail.com>
2021-08-29 17:35:25 -0500

    Form Validation now works

--- my-app/src/components/imperial.js
+++ my-app/src/components/imperial.js
@@ -6,17 +6,26 @@ import StyledButton from "./styling-components/submitbutton";
 import StyledForm from "./styling-components/form";
 import FormPositioner from "./styling-components/formpositioner";
 import StyledHomeLink from "./styling-components/homebutton";
+import * as yup from 'yup';
+import schema from './formvalidation/formschema';
 
 
-const initialFormValues = {
-    height: 0,
-    weight: 0
-};
+
+
+const initialFormErrors = {
+    weight: '', 
+    height: ''
+  }
+
+  const initialDisabled = true
+
 
 const ImperialForm = () => {
-    const [formValue, setFormValue] = useState(initialFormValues);
+    const [formValue, setFormValue] = useState([]);
     const [bmi, setBmi] = useState(0)
     const [health, setHealth] = useState('');
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
+    const [disabled, setDisabled] = useState(initialDisabled) 
 
     const healthTest = () => {
         if (bmi < 18) {
@@ -30,27 +39,46 @@ const ImperialForm = () => {
         }
     }
 
+    const validate = (name, value) => {
+        yup.reach(schema, name)
+          .validate(value)
+          .then(() => setFormErrors({...formErrors, [name]: '' }))
+          .catch(err => setFormErrors({...formErrors, [name]: err.errors[0] }))
+      }
+
     const change = (evt) => {
         const { type, name, value, checked } = evt.target;
 
         const newValue = type === "checkbox" ? checked : value;
 
-        setFormValue({ ...formValue, [name]: newValue });
+        validate(name, newValue)
+
+        if(newValue >= 0) {
+            setFormValue({ ...formValue, [name]: newValue });
+        }
     }
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
         setBmi(Math.round((formValue.weight / (formValue.height * formValue.height)) * 703))
+       
     }
 
     useEffect(() => {
-      healthTest()      
+        healthTest();
     })
+
+    useEffect(() => {
+        schema.isValid(formValue).then(valid => setDisabled(!valid));
+      }, [formValue])
+
 
     return (
         <Background>
             <FormContainer>
                 <h1>B.M.I. Calculator</h1>
+                <h2>{formErrors.height}</h2>
+                <h2>{formErrors.weight}</h2>
                 <StyledForm  onSubmit={handleSubmit}>
                     <FormPositioner>
                         <StyledLabel>Height(in):</StyledLabel>
@@ -69,7 +97,7 @@ const ImperialForm = () => {
                             placeholder='Enter your weight!'
                             onChange={change}
                         /><br></br>
-                        <StyledButton>Submit</StyledButton>
+                        <StyledButton disabled={disabled}>Submit</StyledButton>
                     </FormPositioner>
                 </StyledForm>
                 <h1>You have a B.M.I. of {bmi}</h1>
--- my-app/src/components/metric.js
+++ my-app/src/components/metric.js
@@ -6,16 +6,22 @@ import StyledButton from "./styling-components/submitbutton";
 import StyledForm from "./styling-components/form";
 import FormPositioner from "./styling-components/formpositioner";
 import StyledHomeLink from "./styling-components/homebutton";
+import * as yup from 'yup';
+import schema from './formvalidation/formschema';
 
-const initialFormValues = {
-    height: 0,
-    weight: 0
-};
+const initialFormErrors = {
+    weight: '', 
+    height: ''
+  }
+
+  const initialDisabled = true
 
 const MetricForm = () => {
-    const [formValue, setFormValue] = useState(initialFormValues);
+    const [formValue, setFormValue] = useState([]);
     const [bmi, setBmi] = useState(0)
     const [health, setHealth] = useState('');
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
+    const [disabled, setDisabled] = useState(initialDisabled) 
 
     const healthTest = () => {
         if (bmi < 18) {
@@ -29,27 +35,46 @@ const MetricForm = () => {
         }
     }
 
+    const validate = (name, value) => {
+        yup.reach(schema, name)
+          .validate(value)
+          .then(() => setFormErrors({...formErrors, [name]: '' }))
+          .catch(err => setFormErrors({...formErrors, [name]: err.errors[0] }))
+      }
+
     const change = (evt) => {
         const { type, name, value, checked } = evt.target;
 
         const newValue = type === "checkbox" ? checked : value;
 
-        setFormValue({ ...formValue, [name]: newValue });
+        validate(name, newValue)
+
+        if(newValue >= 0) {
+            setFormValue({ ...formValue, [name]: newValue });
+        }
     }
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
         setBmi(Math.round((formValue.weight / (formValue.height * formValue.height)) * 10000))
+        
     }
 
     useEffect(() => {
-      healthTest()      
+        healthTest();
     })
+
+    useEffect(() => {
+        schema.isValid(formValue).then(valid => setDisabled(!valid));
+      }, [formValue])
+
 
     return (
         <Background>
             <FormContainer>
                 <h1>B.M.I. Calculator</h1>
+                <h2>{formErrors.height}</h2>
+                <h2>{formErrors.weight}</h2>
                 <StyledForm  onSubmit={handleSubmit}>
                     <FormPositioner>
                         <StyledLabel>Height(cm):</StyledLabel>
@@ -68,7 +93,7 @@ const MetricForm = () => {
                             placeholder='Enter your weight!'
                             onChange={change}
                         /><br></br>
-                        <StyledButton>Submit</StyledButton>
+                        <StyledButton disabled={disabled}>Submit</StyledButton>
                     </FormPositioner>
                 </StyledForm>
                 <h1>You have a B.M.I. of {bmi}</h1>
