a297fe4f1229272a9c4e60d7a64a1ced8e27d006
onuri5 <jomibaofficial@gmail.com>
2021-08-28 21:11:52 -0500

    finished setting up the background for the app

---
+++ my-app/src/homepage.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const MainPage = () => {
+    const Background = styled.div `
+        background-image: radial-gradient(circle at top left, #51A1DD 30%, #0053B5);
+        height: 100vh;
+        font-size: 62.5%;
+    `
+
+    return (
+        <Background>
+            
+        </Background>
+    )
+}
+
+export default MainPage;
